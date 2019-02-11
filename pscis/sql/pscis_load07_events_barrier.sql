236ffa1a9a4f24dd8c9e9d7234b44cdd621b0e5d
Simon Norris <snorris@hillcrestgeo.ca>
2019-02-11 11:06:34 -0800

    tidy up barrier table, including geometry and inferred habitat confirmation status

--- pscis/sql/pscis_load07_events_barrier.sql
+++ pscis/sql/pscis_load07_events_barrier.sql
@@ -3,11 +3,19 @@ DROP TABLE IF EXISTS whse_fish.pscis_events_barrier;
 CREATE TABLE whse_fish.pscis_events_barrier AS
 SELECT
   e.*,
-  p.current_pscis_status,
-  p.current_barrier_result_code
+  CASE
+    WHEN hc.stream_crossing_id IS NOT NULL
+    THEN 'HABITAT CONFIRMATION'
+    ELSE p.current_pscis_status
+  END AS pscis_status,
+  p.current_barrier_result_code,
+  -- include the source geometry just to make things simple
+  p.geom
 FROM whse_fish.pscis_events e
 INNER JOIN whse_fish.pscis_points_all p
 ON e.stream_crossing_id = p.stream_crossing_id
-WHERE current_barrier_result_code IN ('BARRIER', 'POTENTIAL');
+LEFT OUTER JOIN whse_fish.pscis_habitat_confirmation_svw hc
+ON e.stream_crossing_id = hc.stream_crossing_id
+WHERE p.current_barrier_result_code IN ('BARRIER', 'POTENTIAL');
 
 ALTER TABLE whse_fish.pscis_events_barrier ADD PRIMARY KEY (stream_crossing_id);
